1b93a8241d672576cd1883e8a4466ddc9372963e
KacperFigaWSEI <147261375+KacperFigaWSEI@users.noreply.github.com>
2023-10-07 17:38:02 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ console.log('hello');
 const registryInputs = document.querySelectorAll('.register-form > input');
 
 const registrySubmit = document.querySelector('.register-form > button');
+
+const errorBoxes = document.querySelectorAll('.error-message');
+console.log(errorBoxes)
+const registerObj = {}
+
+const validateRegisry = (e) => {
+    e.preventDefault();
+    registryInputs.forEach((register,i)=>{
+        console.log(register.value, register.dataset.name);
+        registerObj[register.dataset.name] = register.value;
+        if(!register.value){
+          console.log(errorBoxes[i])
+        }
+    })
+
+console.log(registerObj);
+}
+
+registrySubmit.addEventListener('click', validateRegisry);
