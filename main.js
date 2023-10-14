591494676cff840fe2eed2b99b11eaee7ed664dd
KacperFiga <kacper.figa.com@o2.pl>
2023-10-14 18:08:45 +0200

    validate

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-console.log("hello");
+const registryInputs = document.querySelectorAll(
+  ".register-form > div > input"
+);
 
-const registryInputs = document.querySelectorAll(".register-form > input");
-
-const registrySubmit = document.querySelector(".register-form > button");
+const registrySubmit = document.querySelector(".submit-register");
 
 const errorBoxes = document.querySelectorAll(".error-message");
+
+const errorObj = (err) => {
+  switch (err) {
+    case "EMPTY":
+      return "Wprowadź wartość dla ";
+      break;
+    case "TYPE_ERROR":
+      return "Błędna wartość, wprwadź wartość w systemie heksadecymalnym";
+      break;
+    default:
+      console.log("XD");
+  }
+};
+
+const registerObj = {};
 
 const validateRegisry = (e) => {
   e.preventDefault();
@@ -12,7 +27,10 @@ const validateRegisry = (e) => {
     console.log(register.value, register.dataset.name);
     registerObj[register.dataset.name] = register.value;
     if (!register.value) {
-      console.log(errorBoxes[i]);
+      errorBoxes[i].textContent = errorObj("EMPTY") + register.dataset.name;
+    } else if (!/^#[0-9A-F]{6}[0-9a-f]{0,2}$/i.test(register.value)) {
+      console.log("truftuut");
+      errorBoxes[i].textContent = errorObj("TYPE_ERROR");
     }
   });
 
